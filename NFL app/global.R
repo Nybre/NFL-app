ef671ea824caa8502b42de74b6d09e4398a710c1
Nybre <g11m1065trader@gmail.com>
2021-10-01 19:46:42 +0200

    Update global.R

--- NFL app/global.R
+++ NFL app/global.R
@@ -12,8 +12,7 @@ library(tidyverse)
 library(ggimage)
 library(DT)
 
-source("modules/Dashboard_file.R",local=TRUE)  
-source("modules/Dashboard_file_1.R",local=TRUE)  
+source("modules/Dashboard_file.R",local=TRUE)    
 
 pace_data = read.csv(paste(getwd(),"data_folder/pace_data.csv", sep = "/"))
 
