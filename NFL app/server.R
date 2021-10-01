e5a06cd6b877a67b6d4743d666af45b225ae9ba8
Nybre <g11m1065trader@gmail.com>
2021-10-01 19:46:11 +0200

    Update server.R

--- NFL app/server.R
+++ NFL app/server.R
@@ -3,6 +3,5 @@ options(digits.secs = 3) # Include milliseconds in time display
 shinyServer(function(input, output,session) {
   
   #----------------server modules------------------
-  callModule(Dashboard_file, "Dashboard_file-module", pool) 
-  callModule(Dashboard_file, "Dashboard_file_1-module", pool) 
+  callModule(Dashboard_file, "Dashboard_file-module", pool)   
 })
